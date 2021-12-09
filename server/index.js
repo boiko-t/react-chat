7c4bcc75e831f37f7e3e660d9fab104c3269cbbb
Taisa Mytiuk <taisamytiuk@gmail.com>
2021-12-09 17:42:09 +0200

    refactor server: add routes

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import bp from 'body-parser';
-import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
-import { verifyUser } from './auth.js';
-import { addUserIfNotExists, getUserById } from './db.js';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
+import { getUserByToken, addGoogleUser } from './routes/auth.js';
 dotenv.config();
 
 const hostname = '127.0.0.1';
@@ -26,55 +24,9 @@ server.use(cookieParser());
 server.use(bp.urlencoded({ extended: true }));
 server.use(bp.json());
 
-const validateJWT = (token) => {
-  if (!token) return null;
+server.get('/api/get-user', getUserByToken);
 
-  try {
-    const result = jwt.verify(token, process.env.TOKEN_SECRET);
-    return result.username;
-  } catch (e) {
-    return null;
-  }
-};
-
-function generateAccessToken(userId) {
-  return jwt.sign(userId, process.env.TOKEN_SECRET, { expiresIn: '86400s' });
-}
-
-server.get('/api/get-user', async (req, res) => {
-  const userId = validateJWT(req.cookies.JWT);
-
-  if (userId) {
-    const user = await getUserById(userId);
-    res.status(200).json(user);
-  } else {
-    res.sendStatus(403);
-  }
-});
-
-server.post('/api/login-google', async (req, res) => {
-  const userId = validateJWT(req.cookies.JWT);
-
-  let dbUser;
-  if (userId) {
-    dbUser = await getUserById(userId);
-  } else {
-    const googleUser = await verifyUser(req.body.token);
-    dbUser = await addUserIfNotExists(googleUser);
-  }
-
-  const token = generateAccessToken({ username: dbUser._id });
-
-  res.cookie('JWT', token, {
-    maxAge: 86_400_000,
-    httpOnly: true,
-    secure: true,
-    sameSite: 'none',
-  });
-
-  res.status(201);
-  res.json(dbUser);
-});
+server.post('/api/login-google', addGoogleUser);
 
 server.get('/', (req, res) => {
   res.json('Hello');
---
+++ server/routes/auth.js
@@ -0,0 +1,65 @@
+import { OAuth2Client } from 'google-auth-library';
+import jwt from 'jsonwebtoken';
+import { addUserIfNotExists, getUserById } from '../db.js';
+
+const client = new OAuth2Client(process.env.CLIENT_ID);
+
+const verifyAccessToken = (token) => {
+  if (!token) return null;
+
+  try {
+    const result = jwt.verify(token, process.env.TOKEN_SECRET);
+    return result.username;
+  } catch (e) {
+    return null;
+  }
+};
+
+function generateAccessToken(userId) {
+  return jwt.sign(userId, process.env.TOKEN_SECRET, { expiresIn: '86400s' });
+}
+
+const verifyGoogleUser = async (token) => {
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.CLIENT_ID,
+  });
+  const { name, email, picture } = ticket.getPayload();
+
+  return { name, email, picture };
+};
+
+export const getUserByToken = async (req, res) => {
+  const userId = verifyAccessToken(req.cookies.JWT);
+
+  if (userId) {
+    const user = await getUserById(userId);
+    res.status(200).json(user);
+  } else {
+    res.sendStatus(403);
+  }
+};
+
+export const addGoogleUser = async (req, res) => {
+  const userId = verifyAccessToken(req.cookies.JWT);
+
+  let dbUser;
+  if (userId) {
+    dbUser = await getUserById(userId);
+  } else {
+    const googleUser = await verifyGoogleUser(req.body.token);
+    dbUser = await addUserIfNotExists(googleUser);
+  }
+
+  const token = generateAccessToken({ username: dbUser._id });
+
+  res.cookie('JWT', token, {
+    maxAge: 86_400_000,
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+  });
+
+  res.status(201);
+  res.json(dbUser);
+};
